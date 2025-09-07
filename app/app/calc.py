399b1a59976a3b2c4f1e5944598f79ae4fad70cd
sandeep-soni_dentsu <sandeep.soni@dentsu.com>
2025-09-07 18:53:18 +0530

    example test

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Function
+"""
+
+def add(x, y):
+    """Add x and y and return result"""
+    return x + y
+
+def substract(x,y):
+    """Substract numbers"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Tests adding numbers together."""
+        res = calc.add(10, 20)
+        self.assertEqual(res, 30)
+
+    def test_substract_numbers(self):
+        """Test Substract numbers"""
+        res = calc.substract(30,10)
+        self.assertEqual(res, 20)
